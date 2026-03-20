fa1c20c6dec248924a4c4339519591962a68146f
Richard <legalize@xmission.com>
2026-03-19 22:55:14 -0600

    Disassemble DIV, MPY instructions

---
+++ ND812/scripts/test/div.asm
@@ -0,0 +1,7 @@
+/ div.bin
+/
+/ record 1
+/ file offset: 0x0
+        *0000
+        DIV                             / 1001
+/ checksum: 6777
---
+++ ND812/scripts/test/mpy.asm
@@ -0,0 +1,7 @@
+/ mpy.bin
+/
+/ record 1
+/ file offset: 0x0
+        *0000
+        MPY                             / 1000
+/ checksum: 7000
