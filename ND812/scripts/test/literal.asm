a15d44a6451ce0eec566e93c42197f3e75e4a251
Richard <legalize@xmission.com>
2026-03-20 08:43:18 -0600

    Add test cases for ANDL, ADDL, SUBL instructions

---
+++ ND812/scripts/test/literal.asm
@@ -0,0 +1,18 @@
+/ literal.bin
+/
+/ record 1
+/ file offset: 0x0
+        *0000
+        ANDL    00                      / 2100
+        ANDL    01                      / 2101
+        ANDL    37                      / 2137
+        ANDL    77                      / 2177
+        ADDL    00                      / 2200
+        ADDL    01                      / 2201
+        ADDL    37                      / 2237
+        ADDL    77                      / 2277
+        SUBL    00                      / 2300
+        SUBL    01                      / 2301
+        SUBL    37                      / 2337
+        SUBL    77                      / 2377
+/ checksum: 4343
