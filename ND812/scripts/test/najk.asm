c3b6e275ebda4b792ac6d242dd4df602cfe31cb5
Richard <legalize@xmission.com>
2026-03-19 22:15:52 -0600

    Disassemble NAJK instruction

---
+++ ND812/scripts/test/najk.asm
@@ -0,0 +1,9 @@
+/ najk.bin
+/
+/ record 1
+/ file offset: 0x0
+        *0000
+        NAJK    J                       / 1130
+        NAJK    K                       / 1230
+        NAJK    JK                      / 1330
+/ checksum: 4070
