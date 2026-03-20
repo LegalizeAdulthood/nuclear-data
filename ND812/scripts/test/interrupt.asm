3212506c67cf5324fc20043a2c04a581fc6fe548
Richard <legalize@xmission.com>
2026-03-20 08:33:32 -0600

    Test interrupt instructions

---
+++ ND812/scripts/test/interrupt.asm
@@ -0,0 +1,10 @@
+/ interrupt.bin
+/
+/ record 1
+/ file offset: 0x0
+        *0000
+        IONH                            / 1004
+        IONA                            / 1006
+        IONB                            / 1005
+        IOFF                            / 1003
+/ checksum: 3756
